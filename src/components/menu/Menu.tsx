0beeb3e255228b7f6824b654800e52cdb7e9bc00
Ryota NIINOMI Takemoto <r21nomi@gmail.com>
2026-05-09 08:22:26 +0900

    Add query param to switch artworks

--- src/components/menu/Menu.tsx
+++ src/components/menu/Menu.tsx
@@ -1,14 +1,21 @@
 import styles from './Menu.module.scss'
 import React from 'react'
 import { useArts } from '@/hooks/contexts/artsContext'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 export type IMenu = React.ComponentPropsWithRef<'div'>
 
 const Menu: React.FC<IMenu> = ({ className }) => {
   const { arts, setIndex } = useArts()
+  const router = useRouter()
+  const searchParams = useSearchParams()
 
   const onClicked = (index: number) => {
     setIndex(index)
+    const id = arts[index].id
+    const params = new URLSearchParams(searchParams.toString())
+    params.set('id', id)
+    router.push(`?${params.toString()}`)
   }
 
   return (
